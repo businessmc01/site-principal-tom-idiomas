d27a55cf19d1f31b481873a0e09c700973f6174e
businessmc01 <91619324+businessmc01@users.noreply.github.com>
2021-10-06 10:20:04 -0300

    Add files via upload

---
+++ source/app.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-icon").addEventListener("click",()=>{
+    document.querySelector(".header__nav").classList.toggle("show-sidebar")
+})
+
+document.querySelector(".close-btn").addEventListener("click",()=>{
+    document.querySelector(".header__nav").classList.toggle("show-sidebar")
+})
